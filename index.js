5fa87411a24d5da360cf439abf8c8735370b1d7b
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2019-10-16 20:40:30 +0100

    module: cleanup

--- index.js
+++ index.js
@@ -3,12 +3,6 @@
 Object.defineProperty(exports, '__esModule', { value: true });
 
 class SeamlessClip extends HTMLElement {
-  constructor(src) {
-    super();
-
-    this.src = src;
-  }
-
   get src() {
     return this.getAttribute('src')
   }
@@ -21,23 +15,31 @@ class SeamlessClip extends HTMLElement {
 }
 
 class Seamless extends HTMLVideoElement {
-  constructor(encoding = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"') {
-    super();
+  get encoding() {
+    if (this.hasAttribute('encoding')) {
+      return this.getAttribute('encoding')
+    }
 
-    // A bit of a pain, eg. 'video/webm; codecs="vorbis, vp8"'
-    // More, https://tools.ietf.org/html/rfc6381#section-3.3
-    this.encoding = encoding;
+    // A bit of a pain the codec part, another option might be
+    // 'video/webm; codecs="vorbis, vp8"'
+    return 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+  }
+
+  set encoding(v) {
+    if (v) {
+      this.setAttribute('encoding', v);
+    }
   }
 
   connectedCallback() {
-    const ns = this.getAttribute('is');
+    const name = this.hasAttribute('is') ? this.getAttribute('is') : 'very-seamless';
 
     // Allow for attaching event listeners in time
-    window.customElements.whenDefined(ns).then(() => {
+    window.customElements.whenDefined(name).then(() => {
       // Make sure fetching avoided unless tag has context
       if (this.isConnected) {
         // Collect `src` urls, tracks only
-        const children = this.querySelectorAll(`${ns}-clip[src]`);
+        const children = this.querySelectorAll(`${name}-clip[src]`);
         const assets = Array.from(children).map(o => o.getAttribute('src'));
 
         this.render(...assets).catch(({ message }) => {
@@ -52,8 +54,10 @@ class Seamless extends HTMLVideoElement {
   async render(...assets) {
     // Bail out quick
     if (MediaSource.isTypeSupported(this.encoding) === false) {
-      throw Error('Unsupported mime type / codec')
+      throw MediaError({ code: 3, message: 'Unsupported mime type / codec' })
     }
+
+    const mediaSource = new MediaSource();
 
     // Collect errors locally
     const promises = assets.map(asset => fetch(asset)
@@ -63,19 +67,11 @@ class Seamless extends HTMLVideoElement {
 
     // Batch download
     const resultsMaybe = await Promise.all(promises);
-    const results = resultsMaybe
-      // Drop errors + blanks
-      .filter(result => !(result instanceof Error))
-      .filter(result => !!result)
-      // Format data
-      .map(buffer => new Uint8Array(buffer));
+    const results = resultsMaybe.filter(o => !(o instanceof Error)).map(o => new Uint8Array(o));
 
     if (results.length === 0) {
-      throw Error('Nothing to play back')
+      throw MediaError({ code: 4, message: 'Nothing to play back' })
     }
-
-    const mediaSource = new MediaSource();
-    const { URL } = window;
 
     mediaSource.addEventListener('sourceopen', () => {
       const sourceBuffer = mediaSource.addSourceBuffer(this.encoding);
@@ -94,10 +90,13 @@ class Seamless extends HTMLVideoElement {
         }
       });
 
-      URL.revokeObjectURL(this.src);
-    });
+      window.URL.revokeObjectURL(this.src);
+    }, { once: true });
 
-    this.src = URL.createObjectURL(mediaSource);
+    // This makes Safari flicker on first play. Using a source element seemed to help, but
+    // somehow won't trigger the `sourceopen` event, which however Safari
+    // don't need for playback anyway? Autoplay related maybe?
+    this.src = window.URL.createObjectURL(mediaSource);
   }
 }
 
