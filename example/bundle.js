b4cac23669a82aab2c39d7028cc48d2b8b26cb06
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2019-10-16 20:40:37 +0100

    example: rebuild

--- example/bundle.js
+++ example/bundle.js
@@ -2,12 +2,6 @@
   'use strict';
 
   class SeamlessClip extends HTMLElement {
-    constructor(src) {
-      super();
-
-      this.src = src;
-    }
-
     get src() {
       return this.getAttribute('src')
     }
@@ -20,23 +14,31 @@
   }
 
   class Seamless extends HTMLVideoElement {
-    constructor(encoding = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"') {
-      super();
+    get encoding() {
+      if (this.hasAttribute('encoding')) {
+        return this.getAttribute('encoding')
+      }
 
-      // A bit of a pain, eg. 'video/webm; codecs="vorbis, vp8"'
-      // More, https://tools.ietf.org/html/rfc6381#section-3.3
-      this.encoding = encoding;
+      // A bit of a pain the codec part, another option might be
+      // 'video/webm; codecs="vorbis, vp8"'
+      return 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'
+    }
+
+    set encoding(v) {
+      if (v) {
+        this.setAttribute('encoding', v);
+      }
     }
 
     connectedCallback() {
-      const ns = this.getAttribute('is');
+      const name = this.hasAttribute('is') ? this.getAttribute('is') : 'very-seamless';
 
       // Allow for attaching event listeners in time
-      window.customElements.whenDefined(ns).then(() => {
+      window.customElements.whenDefined(name).then(() => {
         // Make sure fetching avoided unless tag has context
         if (this.isConnected) {
           // Collect `src` urls, tracks only
-          const children = this.querySelectorAll(`${ns}-clip[src]`);
+          const children = this.querySelectorAll(`${name}-clip[src]`);
           const assets = Array.from(children).map(o => o.getAttribute('src'));
 
           this.render(...assets).catch(({ message }) => {
@@ -51,8 +53,10 @@
     async render(...assets) {
       // Bail out quick
       if (MediaSource.isTypeSupported(this.encoding) === false) {
-        throw Error('Unsupported mime type / codec')
+        throw MediaError({ code: 3, message: 'Unsupported mime type / codec' })
       }
+
+      const mediaSource = new MediaSource();
 
       // Collect errors locally
       const promises = assets.map(asset => fetch(asset)
@@ -62,19 +66,11 @@
 
       // Batch download
       const resultsMaybe = await Promise.all(promises);
-      const results = resultsMaybe
-        // Drop errors + blanks
-        .filter(result => !(result instanceof Error))
-        .filter(result => !!result)
-        // Format data
-        .map(buffer => new Uint8Array(buffer));
+      const results = resultsMaybe.filter(o => !(o instanceof Error)).map(o => new Uint8Array(o));
 
       if (results.length === 0) {
-        throw Error('Nothing to play back')
+        throw MediaError({ code: 4, message: 'Nothing to play back' })
       }
-
-      const mediaSource = new MediaSource();
-      const { URL } = window;
 
       mediaSource.addEventListener('sourceopen', () => {
         const sourceBuffer = mediaSource.addSourceBuffer(this.encoding);
@@ -93,10 +89,13 @@
           }
         });
 
-        URL.revokeObjectURL(this.src);
-      });
+        window.URL.revokeObjectURL(this.src);
+      }, { once: true });
 
-      this.src = URL.createObjectURL(mediaSource);
+      // This makes Safari flicker on first play. Using a source element seemed to help, but
+      // somehow won't trigger the `sourceopen` event, which however Safari
+      // don't need for playback anyway? Autoplay related maybe?
+      this.src = window.URL.createObjectURL(mediaSource);
     }
   }
 
@@ -117,10 +116,11 @@
 
   const video = document.querySelector('video');
 
-  video.appendChild(source);
   video.addEventListener('error', (e) => {
     console.log('oops!', e.message);
   });
+
+  video.appendChild(source);
 
   /* eslint func-style: warn */
   function createPlaylist(...list) {
@@ -128,12 +128,12 @@
       // Get full path
       .map(item => document.location.href + item)
       // Add duration for each clip
-      .reduce((crop, item) => crop.concat('#EXTINF:10', item), [
+      .reduce((crop, item) => crop.concat('#EXTINF:1', item), [
         // Head
         '#EXTM3U',
         '#EXT-X-VERSION:3',
         '#EXT-X-MEDIA-SEQUENCE:0',
-        '#EXT-X-TARGETDURATION:10'
+        '#EXT-X-TARGETDURATION:2'
       ])
       // Foot
       .concat('#EXT-X-ENDLIST')
