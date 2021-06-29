c6aa6b0b77757190e71e58e96a5adecf1ee64a1e
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-29 20:17:12 +0100

    test: fix for dprint

--- test.js
+++ test.js
@@ -1,44 +1,44 @@
-import '@ungap/custom-elements-builtin'
-import 'cutaway'
-import { report, assert } from 'tapeless'
-import { Seamless, SeamlessClip } from './index.mjs'
+import "@ungap/custom-elements"
+import "cutaway"
+import { assert, report } from "tapeless"
+import { Seamless, SeamlessClip } from "./main.js"
 
 const { ok, notOk, equal } = assert
 
 equal
-  .describe('will export')
-  .test(typeof Seamless, 'function')
-  .test(typeof SeamlessClip, 'function')
+  .describe("will export")
+  .test(typeof Seamless, "function")
+  .test(typeof SeamlessClip, "function")
 
 try {
-  customElements.define('very-seamless', Seamless, { extends: 'video' })
+  customElements.define("very-seamless", Seamless, { extends: "video" })
 } catch (e) {
   ok
-    .describe('will define `&lt;very-seamless&gt;`')
+    .describe("will define `&lt;very-seamless&gt;`")
     .test(e)
 }
 
-const video = document.createElement('video', { is: 'very-seamless' })
+const video = document.createElement("video", { is: "very-seamless" })
 
 ok
-  .describe('tag known')
+  .describe("tag known")
   .test(video instanceof HTMLElement)
 
 notOk
-  .describe('tag not unknown')
+  .describe("tag not unknown")
   .test(video instanceof HTMLUnknownElement)
 
-video.style.display = 'none'
-video.muted = 'muted'
+video.style.display = "none"
+video.muted = "muted"
 video.width = 320
 video.height = 240
 
 equal
-  .describe('will default encoding')
+  .describe("will default encoding")
   .test(video.encoding, 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"')
 
 Array.from({ length: 4 })
-  .map(() => 'https://cors-anywhere.herokuapp.com/https://github.com/thewhodidthis/seamless')
+  .map(() => "https://cors-anywhere.herokuapp.com/https://github.com/thewhodidthis/seamless")
   .map((a, i) => `${a}/raw/master/example/assets/fragment-${i}.mp4`)
   .forEach((src) => {
     const clip = new SeamlessClip()
@@ -49,17 +49,16 @@ Array.from({ length: 4 })
   })
 
 document.body.appendChild(video)
-
 ;(async () => {
   try {
     await video.play()
 
     ok
-      .describe('will play')
+      .describe("will play")
       .test(true)
   } catch (e) {
     ok
-      .describe('unable to trigger play')
+      .describe("unable to trigger play")
       .test(e instanceof Error)
   } finally {
     report()
